d10dbdffff1746ba4ec5894d5013ba6d5a1694ce
jonathn6 <jonathn6@icloud.com>
2021-11-13 13:08:38 -0500

    Todo complete - all tests pass

---
+++ PalindromeExercise/PalindromeExercise/WordSmith .cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome (string forwardWord)
+        {
+            bool returnValue = false;
+            string backwardsWord = "";
+
+            forwardWord = forwardWord.ToUpper().Replace(" ","");
+
+            var wordLength = forwardWord.Length;
+            char[] textarray = forwardWord.ToCharArray();
+
+            for (var i = wordLength-1; i >= 0; i--)
+            {
+                backwardsWord = backwardsWord + textarray[i];
+            }
+
+            if (forwardWord == backwardsWord)
+            {
+                returnValue = true;
+            }
+
+            return returnValue;
+
+        }
+
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,24 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("redivider", true)]
+        [InlineData("deified", true)]
+        [InlineData("civic", true)]
+        [InlineData("radar", true)]
+        [InlineData("taco cat", true)]
+        [InlineData("my gym", true)]
+        [InlineData("jonathan", false)]
+        [InlineData("TrueCoders", false)]
+        public void IsAPalindrome(string word, bool expected)
         {
-
+            //Arrange
+            var test = new WordSmith();
+            //Act
+            var actual = test.IsAPalindrome(word);
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
